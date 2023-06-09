8667c74b4e97981b1376ee297cb5190f5aff5f2d
Rafael Oliveira <oliveirafael@icloud.com>
2023-06-09 17:11:45 -0300

    Initial Commit

---
+++ CreatingCellsWithTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreatingCellsWithTime
+//
+//  Created by Rafael Oliveira on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
